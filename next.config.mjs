92a4190895d0a30d80181fddff1ffc164ff265ba
TAlam <tanvir.alam.shawn@gmail.com>
2022-11-20 22:31:57 +0100

    Responsive header componenent

--- next.config.mjs
+++ next.config.mjs
@@ -13,5 +13,8 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    domains: ["miro.medium.com", "image.tmdb.org"]
+  }
 };
 export default config;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import React from 'react'
+import HeaderItems from './HeaderItems'
+
+import { 
+  CheckIcon,
+  BookmarkIcon,
+  HomeIcon,
+  BoltIcon,
+  EyeIcon,
+  UserIcon
+} from '@heroicons/react/24/outline'
+
+const Header = () => {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItems title="HOME" Icon={HomeIcon} />
+        <HeaderItems title="HOME" Icon={CheckIcon} />
+        <HeaderItems title="HOME" Icon={BookmarkIcon} />
+        <HeaderItems title="HOME" Icon={BoltIcon} />
+        <HeaderItems title="HOME" Icon={EyeIcon} />
+        <HeaderItems title="HOME" Icon={UserIcon} />
+      </div>
+      <Image 
+        alt="logo" 
+        className="object-contain"
+        src="/logo.png" 
+        width={300} 
+        height={150} />
+    </header>
+  )
+}
+
+export default Header
+
